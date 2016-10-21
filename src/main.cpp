80774c173e804e07545fd2bada3cca8c4c61544e
chrisvm <christian.velez55@gmail.com>
2016-10-20 22:57:19 -0400

    vk instance creation code added

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,39 @@ private:
 		// init without opengl, will use vulkan
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZEABLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	VDeleter<VkInstance> instance {vkDestroyInstance};
+	void createInstance()
+	{
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		unsigned int glfwExtensionCount = 0;
+		const char** glfwExtensions =
+			glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, instance.replace()) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create vk instance");
+		}
 	}
 
 	void initVulkan()
 	{
-
+		createInstance();
     }
 
     void mainLoop()
